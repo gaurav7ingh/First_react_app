9e3222e6e3b30a578bfcf392a7fbf5b395e06abd
gaurav7ingh <gauravsingh99626@gmail.com>
2022-12-31 16:22:09 +0530

    base react app

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+
+    render() {
+        return <header>
+            <div className="logo">Logo</div>
+            <input />
+        </header>
+    }
+}
+
+// let styles = {
+//     header: {
+//         background: "#03f000"
+//     },
+//     logo: {
+//         fontFamily: "Roboto",
+//         color: "#fff"
+//     }
+// }
+
+export default Header; 
