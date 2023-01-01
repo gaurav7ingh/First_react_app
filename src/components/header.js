1e3e7f7c82ac623f39fb88fc4239fcf7702ac21b
gaurav7ingh <gauravsingh99626@gmail.com>
2023-01-01 18:37:54 +0530

    state in react

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 
 class Header extends Component {
 
-    onInput(event){
-        console.log(event.target.value);
+    state = {
+        input: "",
+        count: 0
+    }
+
+    onInput = (event) => {
+        this.setState({
+            input: event.target.value
+        })
+    }
+
+    addOne = () => {
+        this.setState((state, props) => ({
+            count: state.count + 1
+        }))
     }
 
     render() {
         return <header>
             <div className="logo">Logo</div>
-            <input onChange={this.onInput}/>
+            <input onChange={this.onInput} />
+            <div>{this.state.input}</div>
+            <div>{this.state.count}</div>
+            <button onClick={()=>this.addOne()}>Add One</button>
         </header>
     }
 }
