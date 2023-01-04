9a1a58f51aa9e0c60bfd33d1371e46b006536b05
gaurav7ingh <gauravsingh99626@gmail.com>
2023-01-04 21:59:29 +0530

    lifecycle in react

---
+++ src/components/lifeCycle.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Life extends Component{
+    render(){
+        return(
+            <h1>
+                This is life cycle
+            </h1>
+        )
+    }
+
+}
+
+export default Life;
